f0197730999538db2a08e490d3b8f9b50885f01d
Emerson Lima <emersonlimago123@gmail.com>
2024-07-12 14:24:21 -0300

    Added rook movement

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -46,6 +47,15 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There are no possible moves for the chosen piece.");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to he target position");
+		};
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
